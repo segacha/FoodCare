f77e0236e7ee6ebcff1c5c79f3adb42a9856fa9c
Emilio Segale <emilolo882@hotmail.com>
2024-05-08 18:33:19 +0200

    vs commit

---
+++ backend/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+const multer = require("multer");
+
+const app = express();
+app.use(cors());
+
+const CONNECTION_STRING = "mongodb+srv://foodcare:webtech2@foodcare.gygzrc9.mongodb.net/?retryWrites=true&w=majority&appName=FoodCare";
+const DATABASE_NAME = "foodcaredb";
+
+let database;
+
+app.listen(3000, async () => {
+    try {
+        const client = await MongoClient.connect(CONNECTION_STRING);
+        database = client.db(DATABASE_NAME);
+        console.log("MongoDB Connection granted");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+});
+
+app.get('/api/foodcare/GetProducts', async (request, response) => {
+    try {
+        const result = await database.collection("products").find({}).toArray();
+        response.send(result);
+    } catch (error) {
+        console.error("Error fetching products:", error);
+        response.status(500).send("Internal Server Error");
+    }
+});
+
+app.post("/api/foodcare/AddProduct", multer().none(), async (request, response) => {
+    try {
+        
+        const numOfDocs = await database.collection("products").countDocuments();
+        await database.collection("products").insertOne({
+            id: (numOfDocs + 1).toString(),
+            description: request.body.newProduct
+        });
+
+        response.json("Product Added");
+    } catch (error) {
+        console.error("Error adding Products:", error);
+        response.status(500).send("Error server");
+    }
+});
+
+app.delete("/api/foodcare/DeleteProduct",(request, response)=> {
+    database.collection("products").deleteOne({
+        id:request.query.id        
+    })
+    response.json("Delete Successful")
+});
+
+
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <h2>{{ title }}</h2>
+      <input id="newProduct"/>&nbsp;
+      <button @click="addNewProduct">Add Product</button>
+      <p v-for="product in products" :key="product._id">
+          <b>*{{ product.description }}</b>&nbsp;
+          <button @click="deleteProduct(product.id)">Delete Product</button>
+      </p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+const API_URL = 'http://localhost:3000/';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  setup() {
+      const title = ref("Food Care");
+      const products = ref([]);
+      
+      const refreshData = async () => {
+          try {
+              const response = await axios.get(API_URL + "api/foodcare/GetProducts");
+              products.value = response.data;
+          } catch (error) {
+              console.error('Error fetching notes:', error);
+          }
+      };
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+      const addNewProduct = async () => {
+          try {
+              const newProduct = document.getElementById("newProduct").value;
+              const formData = new FormData();
+              formData.append("newProduct", newProduct);
+
+              const response = await axios.post(API_URL + "api/foodcare/AddProduct", formData);
+              refreshData();
+              alert(response.data);
+          } catch (error) {
+              console.error('Error adding new Product:', error);
+              alert('An error occurred while adding new Product.');
+          }
+      };
+
+      const deleteProduct = async (id) => {
+          try {
+              const response = await axios.delete(API_URL + "api/foodcare/DeleteProduct?id=" + id);
+              refreshData();
+              alert(response.data);
+          } catch (error) {
+              console.error('Error deleting note:', error);
+              alert('An error occurred while deleting note.');
+          }
+      };
+
+      onMounted(() => {
+          refreshData();
+      });
+
+      return {
+          title,
+          products,
+          addNewProduct,
+          deleteProduct
+      };
+  }
+};
+</script>
