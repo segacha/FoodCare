0b8fbecd7efe1da94051eae0bc9b2e0b4d105fa4
Emilio Segale <emilolo882@hotmail.com>
2024-05-22 22:08:19 +0200

    error fix

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
-    <div>
-        <header class="top-section">
-            <nav>
-                <ul>
-                    <li><a href="#">Inicio</a></li>
-                    <li><a href="#">Productos</a></li>
-                    <li><a href="#">Acerca de</a></li>
-                </ul>
-            </nav>
-            <div class="centered-content">
-                <div class="logo-title-container">
-                    <img class="logo" src="./assets/logo.png" alt="Logo de FoodCare">
-                    <h1>{{ title }}</h1>
-                </div>
-            </div>
-            <a href="#" class="account-button">Ingresar</a>
-        </header>
-    </div>
+        <!-- Top section containing the navigation and logo -->
+    <header class="top-section">
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li> <!-- Navigation link to Home -->
+          <li><a href="#product">Product</a></li> <!-- Navigation link to Products -->
+          <li><a href="#about">About us</a></li> <!-- Navigation link to About -->
+        </ul>
+      </nav>
+      <div class="centered-content">
+        <div class="logo-title-container">
+          <img class="logo" src="./assets/logo.png" alt="Logo de FoodCare"> <!-- Company logo -->
+          <h1>{{ title }}</h1> <!-- Title from the component data -->
+        </div>
+      </div>
+      <a href="#" class="account-button" @click="navigateToLogin">Anmelden</a> <!-- Button to navigate to login page -->
+    </header>
+    
     <main class="bottom-section">
       <router-view></router-view> <!-- Placeholder for router views -->
     </main>
