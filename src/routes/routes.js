cf5a63aa0ec4fe8d8386a946c24b464e069d4b80
ManuelFriedrichOp <Manufrie46@gmail.com>
2024-07-08 14:57:09 +0200

    Shopping Page beendet und Statistics angefangen

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,11 +3,13 @@ import HomePage from '../components/HomePage.vue';
 import LoginPage from '../components/LoginPage.vue';
 import WelcomePage from '../components/Welcome_Page.vue';
 import ShoppingPage from '../components/ShoppingPage.vue';
+import StatisticsPage from '../components/StatisticsPage.vue';
 
 const routes = [
   { path: '/', component: WelcomePage }, 
   { path: '/login', component: LoginPage },
   { path: '/home', component: HomePage },
+  { path: '/statistics', component: StatisticsPage },
   { path: '/shopping-list', component: ShoppingPage,}
 ];
 
